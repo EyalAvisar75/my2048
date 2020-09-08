7fe99d15c61fc756fee63fa1595fe3083cb42b5c
Eyal Avisar <enfer0666@yahoo.com>
2020-09-08 07:36:56 +0300

    Initial Commit

---
+++ My2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My2048
+//
+//  Created by eyal avisar on 08/09/2020.
+//  Copyright © 2020 eyal avisar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
